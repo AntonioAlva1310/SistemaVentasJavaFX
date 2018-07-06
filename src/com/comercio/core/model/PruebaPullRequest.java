78d34b3661111af3ab5bef75f55632c76924810e
marcobonilla <marco.bonilla.alvarado@gmail.com>
2018-07-06 15:46:54 -0600

    agregando clase para probar pull request

---
+++ src/com/comercio/core/model/PruebaPullRequest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.comercio.core.model;
+
+/**
+ *
+ * @author USER
+ */
+public class PruebaPullRequest {
+    public void imprimir(){
+        
+        System.out.println("hola mundo");
+    }
+                         
+                          
+}
